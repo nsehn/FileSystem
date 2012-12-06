0e23c88f402f74ab0079bcde141f137dba3755a4
Nisha Sehn <nishasehn@gmail.com>
2012-12-06 13:46:08 -0800

    Add and read items

--- filing.rb
+++ filing.rb
@@ -7,16 +7,18 @@ require 'debugger'
 set :public_folder, File.dirname(__FILE__) + '/Public'
 
 get '/' do 
+	@items = []
+	File.open('list.txt', 'r').each do |line| #Reads list item to display on index.erb. Line 57-63 gets this information from the ruby code and displays it
+		@items << line
+	end
 	erb :index
-	
 end
+
 
 post '/list' do
 	File.open('list.txt', 'a+') do |file|
 		listitem = params[:listitem]
-		file.puts listitem
-		
-		  
+		file.puts listitem		  
 	end
 		redirect '/'
 end  
